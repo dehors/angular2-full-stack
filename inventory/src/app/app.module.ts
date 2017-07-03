0911a8572f5d23dd4e4deba93bd0360840f7a4fe
slam24 <luismartinez242011@gmail.com>
2017-07-03 03:02:00 -0400

    feat: Animate bottom

--- inventory/src/app/app.module.ts
+++ inventory/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +31,8 @@ import { InventarioService } from './inventario/inventario.service';
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [InventarioService],
   bootstrap: [AppComponent]
--- inventory/src/app/inventario/inventario.component.ts
+++ inventory/src/app/inventario/inventario.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.component.html',
   styleUrls: ['./inventario.component.css'],
-  encapsulation: ViewEncapsulation.Native
+  animations: [
+  	trigger('animacion',[
+  		state('inactive',style({
+  			transform: 'scale(1)'
+  		})),
+  		state('active',style({
+  			backgroundColor: '#cfd8dc',
+  			transform: 'scale(1.2)'
+  		})),
+  		transition('inactive => active', animate('100ms ease-in')),
+  		transition('active => inactive', animate('100ms ease-out'))
+  		])
+  ]
 })
 export class InventarioComponent implements OnInit {
-
+	estado = "inactive";
+	estado2 = "inactive";
   constructor() { }
 
   ngOnInit() {
